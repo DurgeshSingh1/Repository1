669b0e24873e770e7745b2cfe98f9eb3c3fe78cb
Durgesh Singh <57593854+DurgeshSingh1@users.noreply.github.com>
2021-10-28 22:58:49 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int fibonacci(int i) {
+
+   if(i == 0) {
+      return 0;
+   }
+	
+   if(i == 1) {
+      return 1;
+   }
+   return fibonacci(i-1) + fibonacci(i-2);
+}
+
+int  main() {
+
+   int i;
+	
+   for (i = 0; i < 10; i++) {
+      printf("%d\t\n", fibonacci(i));
+   }
+	
+   return 0;
+}
